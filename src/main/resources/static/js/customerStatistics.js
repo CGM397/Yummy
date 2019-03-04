74f5de010a6d87133d3ac1ecc742fca5b7e2f00e
161250008 <161250008@smail.nju.edu.cn>
2019-03-04 23:29:19 +0800

    continue statistics.

---
+++ src/main/resources/static/js/customerStatistics.js
@@ -0,0 +1,197 @@
+function setRestaurantOrderReturnList() {
+    clearRestaurantOrderList();
+    var customerId = localStorage.getItem("customerId");
+    var store = showCustomerOrders(customerId);
+    var mapStore = {};
+    for(var i = 0; i < store.length; i++){
+        if(store[i].orderCondition === "已取消"){
+            if(mapStore.hasOwnProperty(store[i].restaurantId))
+                mapStore[store[i].restaurantId] = mapStore[store[i].restaurantId] + 1;
+            else
+                mapStore[store[i].restaurantId] = 1;
+        }
+    }
+    for(var key in mapStore){
+        if(mapStore.hasOwnProperty(key)){
+            var restaurantName = findRestaurantInfoById(key).restaurantName;
+            addRestaurantOrderListRow(restaurantName, mapStore[key]);
+        }
+    }
+}
+
+function setRestaurantOrderList() {
+    clearRestaurantOrderList();
+    var customerId = localStorage.getItem("customerId");
+    var store = showCustomerOrders(customerId);
+    var mapStore = {};
+    for(var i = 0; i < store.length; i++) {
+        if(store[i].orderCondition === "已完成" || store[i].orderCondition === "送货中") {
+            if(mapStore.hasOwnProperty(store[i].restaurantId))
+                mapStore[store[i].restaurantId] = mapStore[store[i].restaurantId] + 1;
+            else
+                mapStore[store[i].restaurantId] = 1;
+        }
+    }
+    for(var key in mapStore) {
+        if(mapStore.hasOwnProperty(key)){
+            var restaurantName = findRestaurantInfoById(key).restaurantName;
+            addRestaurantOrderListRow(restaurantName, mapStore[key]);
+        }
+    }
+}
+
+function setOrderListSortByTime() {
+    clearOrderList();
+    var store = showCustomerOrders(localStorage.getItem("customerId"));
+    for(var i = store.length - 1; i >= 0; i--) {
+        if(store[i].orderCondition === "已完成" || store[i].orderCondition === "送货中") {
+            var orderId = store[i].orderId;
+            var orderTime = store[i].orderTime;
+            orderTime = orderTime.substring(0, orderTime.indexOf("T")) + " " +
+                orderTime.substring(orderTime.indexOf("T") + 1, orderTime.indexOf("."));
+            var content = "";
+            var total = store[i].totalPrice;
+            var condition = store[i].orderCondition;
+            var restaurantId = store[i].restaurantId;
+            var restaurantName = findRestaurantInfoById(restaurantId).restaurantName;
+            var itemName = store[i].items[0].itemName;
+            var itemsAmount = store[i].items.length;
+            content = restaurantName + ": " + itemName + "等" + itemsAmount + "种商品";
+            addOrderListRow(orderId, orderTime, content, total, condition);
+        }
+    }
+}
+
+function setOrderReturnListSortByTime() {
+    clearOrderList();
+    var store = showCustomerOrders(localStorage.getItem("customerId"));
+    for(var i = store.length - 1; i >= 0; i--) {
+        if(store[i].orderCondition === "已取消") {
+            var orderId = store[i].orderId;
+            var orderTime = store[i].orderTime;
+            orderTime = orderTime.substring(0, orderTime.indexOf("T")) + " " +
+                orderTime.substring(orderTime.indexOf("T") + 1, orderTime.indexOf("."));
+            var content = "";
+            var total = store[i].totalPrice;
+            var condition = store[i].orderCondition;
+            var restaurantId = store[i].restaurantId;
+            var restaurantName = findRestaurantInfoById(restaurantId).restaurantName;
+            var itemName = store[i].items[0].itemName;
+            var itemsAmount = store[i].items.length;
+            content = restaurantName + ": " + itemName + "等" + itemsAmount + "种商品";
+            addOrderListRow(orderId, orderTime, content, total, condition);
+        }
+    }
+}
+
+function setOrderListSortByMoney() {
+    clearOrderList();
+    var store = showCustomerOrders(localStorage.getItem("customerId"));
+    for(var i = store.length - 1; i >= 0; i--) {
+        if(store[i].orderCondition === "已完成" || store[i].orderCondition === "送货中") {
+            var orderId = store[i].orderId;
+            var orderTime = store[i].orderTime;
+            orderTime = orderTime.substring(0, orderTime.indexOf("T")) + " " +
+                orderTime.substring(orderTime.indexOf("T") + 1, orderTime.indexOf("."));
+            var content = "";
+            var total = store[i].totalPrice;
+            var condition = store[i].orderCondition;
+            var restaurantId = store[i].restaurantId;
+            var restaurantName = findRestaurantInfoById(restaurantId).restaurantName;
+            var itemName = store[i].items[0].itemName;
+            var itemsAmount = store[i].items.length;
+            content = restaurantName + ": " + itemName + "等" + itemsAmount + "种商品";
+            addOrderListRow(orderId, orderTime, content, total, condition);
+        }
+    }
+}
+
+function setOrderReturnListSortByMoney() {
+    clearOrderList();
+}
+
+function addOrderListRow(orderId, time, content, total, condition) {
+    var table = document.getElementById("orderList");
+    var row = document.createElement("tr");
+
+    var idCell = document.createElement('td');
+    idCell.style.textAlign='center';
+    idCell.style.display='none';
+    idCell.innerHTML = orderId;
+    row.appendChild(idCell);
+
+    var timeCell = document.createElement('td');
+    timeCell.style.textAlign='center';
+    timeCell.innerHTML = time;
+    row.appendChild(timeCell);
+
+    var contentCell = document.createElement('td');
+    contentCell.style.textAlign='left';
+    contentCell.innerHTML = content;
+    row.appendChild(contentCell);
+
+    var totalCell = document.createElement('td');
+    totalCell.style.textAlign='center';
+    totalCell.innerHTML = total;
+    row.appendChild(totalCell);
+
+    var conditionCell = document.createElement('td');
+    conditionCell.style.textAlign='center';
+    conditionCell.innerHTML = condition;
+    row.appendChild(conditionCell);
+
+    var opCell = document.createElement('td');
+    var check = document.createElement('a');
+    check.innerHTML = '查看详情';
+    check.className = 'opBtn';
+    check.href = 'javascript:void(0);';
+    check.onclick = function() {
+        checkOrder(this);
+    };
+    opCell.appendChild(check);
+    row.appendChild(opCell);
+
+    table.appendChild(row);
+}
+
+function addRestaurantOrderListRow(name, num) {
+    var table = document.getElementById("restaurantOrderList");
+    var row = document.createElement("tr");
+
+    var nameCell = document.createElement('td');
+    nameCell.style.textAlign='center';
+    nameCell.innerHTML = name;
+    row.appendChild(nameCell);
+
+    var numCell = document.createElement('td');
+    numCell.style.textAlign='center';
+    numCell.innerHTML = num;
+    row.appendChild(numCell);
+
+    var opCell = document.createElement('td');
+    var check = document.createElement('a');
+    check.innerHTML = '查看详情';
+    check.className = 'opBtn';
+    check.href = 'javascript:void(0);';
+    check.onclick = function() {
+        checkOrder(this);
+    };
+    opCell.appendChild(check);
+    row.appendChild(opCell);
+
+    table.appendChild(row);
+}
+
+function clearRestaurantOrderList() {
+    var table = document.getElementById("restaurantOrderList");
+    var trs = table.rows;
+    for(var i = trs.length - 1; i > 0; i--)
+        table.deleteRow(i);
+}
+
+function clearOrderList() {
+    var table = document.getElementById("orderList");
+    var trs = table.rows;
+    for(var i = trs.length - 1; i > 0; i--)
+        table.deleteRow(i);
+}
