32449db76e811aa6218634c1dfa72faa11530c48
JSTZCGM <980231201@qq.com>
2019-05-26 16:17:16 +0800

    test.

--- src/main/java/edu/nju/Yummy/model/CommodityInfo.java
+++ src/main/java/edu/nju/Yummy/model/CommodityInfo.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
